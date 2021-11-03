6d4fa0229214dcc3df571f05502f619761b8ce3f
madaopincariu <mada_opy@yahoo.ro>
2021-11-03 20:28:17 +0200

    lab 5 2.1

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from UI.Console import runMenu
 from tests.testAll import runAllTests
+from UI.Console2 import meniu
 
 def main():
     runAllTests()
     lista=[]
     runMenu(lista)
+    meniu()
 main()
